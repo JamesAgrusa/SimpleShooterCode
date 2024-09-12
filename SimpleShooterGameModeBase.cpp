9789bcc63f2d1de73e2e1e9f31919bc998b6c732
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-09-12 19:10:34 -0400

    Add files via upload

--- SimpleShooterGameModeBase.cpp
+++ SimpleShooterGameModeBase.cpp
@@ -1,10 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 
 #include "SimpleShooterGameModeBase.h"
 
 
-void ASimpleShooterGameModeBase::PawnKilled(APawn* PawnKilled)
+void ASimpleShooterGameModeBase::PawnKilled(APawn* PawnKilled) 
 {
     
 }
