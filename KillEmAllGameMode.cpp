c495e9509b42d27aeece41a46e2558fe05cb101c
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-09-18 06:33:09 -0400

    Add files via upload

--- KillEmAllGameMode.cpp
+++ KillEmAllGameMode.cpp
@@ -2,6 +2,9 @@
 
 
 #include "KillEmAllGameMode.h"
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
+#include "ShooterAIController.h"
 
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled) 
@@ -11,7 +14,30 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
    APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
    if (PlayerController != nullptr)
    {
-        PlayerController->GameHasEnded(nullptr, false);
+        EndGame(false); // where player loses
    }
+
+   // for loop over shooterAI in world:
+     // is not dead?
+          // return
+     // End Game
+    for(AShooterAIController* Controller : TActorRange<AShooterAIController>(GetWorld()))
+    {
+          if (!Controller->IsDead())
+          {
+               return;
+          }
+    }
+
+    EndGame(true); 
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+     for (AController* Controller : TActorRange<AController>(GetWorld()))
+     {
+          bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+          Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
+     }
 }
 
